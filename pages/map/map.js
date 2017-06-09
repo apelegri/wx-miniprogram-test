5104fd2fedcc62f7329fc721552df0677a458b38
adrien pelegri <pelegriadrien@gmail.com>
2017-06-10 00:11:05 +0800

    map: get and choose location

---
+++ pages/map/map.js
@@ -0,0 +1,69 @@
+// pages/map/map.js
+Page({
+  data: {
+    location:{}// used to manipulate the map in the view
+  },
+  onLoad: function (e) {
+   this.setData({
+     location: { 
+       latitude: "31.232711", 
+       longitude: "121.47575499999994", 
+       scale: "9"
+     },
+    })
+  },
+  getLocation: function(e) {
+    var that = this
+    wx.getLocation({
+      type: 'wgs84',
+      success: function (res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        console.log(res)
+        that.setData({
+          location: { 
+            latitude: latitude, 
+            longitude: longitude,
+            scale: "14",
+          },
+          markers: [{
+            iconPath: "/images/marker.png",
+            id: 0,
+            latitude: latitude,
+            longitude: longitude,
+            width: 33,
+            height: 50,
+            callout: { 
+              content: "My location", 
+              fontSize: 15,
+              borderRadius: 50, 
+              bgColor: "#34495E",
+              color: "#95A5A6",
+              display: "ALWAYS", 
+              padding: 10 
+            }
+          }]      
+        })  
+       }
+     })
+   },
+   tencentLocation: function (e) {
+     var latitude = "23.128994";
+     var longitude = "113.253250";
+     this.setData({
+       location: { 
+         latitude:latitude, 
+         longitude:longitude,
+         scale: "9" 
+       },
+       markers: [{
+         iconPath: "/images/wechat-marker.png",
+         id: 0,
+         latitude:latitude,
+         longitude:longitude,
+         width: 30,
+         height: 50,
+       }]
+     })
+   }
+})
