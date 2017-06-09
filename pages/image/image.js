d4520f0a6cc7694047d413e9730d35feb844ff72
adrien pelegri <pelegriadrien@gmail.com>
2017-06-09 15:27:18 +0800

    image upload from album and save image to album

---
+++ pages/image/image.js
@@ -0,0 +1,56 @@
+Page({
+  data: {
+    src: []
+  },
+  onload: function () {
+    wx.showNavigationBarLoading()
+  },
+  onShow: function () {
+    wx.setNavigationBarTitle({
+      title: 'Change title',
+      success: function (res) {
+        console.log(res)
+      }
+    })
+  },
+  listenerBtnChooseImage: function () {
+    var that = this
+    // Upload an image
+    wx.chooseImage({
+      count: 1,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: function (res) {
+        console.log('success')
+        that.setData({
+          src: res.tempFilePaths
+        })
+        // Get image info
+        wx.getImageInfo({
+          src: res.tempFilePaths[0],
+          success: function (res) {
+            console.log(res.width)
+            console.log(res.height)
+            console.log(res.path)
+          }
+        })
+      }
+    })
+  },
+  imgLongTap: function () {
+    console.log('ok 1')
+    wx.saveImageToPhotosAlbum({
+      filePath: this.data.src[0],
+      success(res) {
+        wx.showToast({
+          title: 'Save',
+          icon: 'success',
+          duration: 1500
+        });
+        wx.reLaunch({
+          url: '/pages/index/index'
+        });
+      }
+    })
+  }
+})
